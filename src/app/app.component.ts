695152e5d7748ea3c1de1981814657c57285418b
Logon PF Fiap <logonpflocal@fiap.com.br>
2020-02-19 22:48:02 -0300

    segunda aula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'segundaAulaAngular2020';
+  images: Array<any> = [
+    {
+      url:"./assets/img/imagem1.jpg",
+      titulo: "Tela",
+      ativa:true
+    },{
+      url:"./assets/img/imagem2.jpg",
+      titulo: "Packman",
+      ativa:true
+    },{
+      url:"./assets/img/imagem3.jpg",
+      titulo: "Porche",
+      ativa:true
+    }
+  ]
+
+  // função para deletar imagem
+  deletar(index:string){
+      this.images[index].ativa = false;
+  }
+
+  // função para adicionar imagem1
+  novaImagem(){
+    let objetoImagem = {
+      titulo: "Imagem aleatória",
+      url: "https://loremflickr.com/320/240?data="+new Date().getTime(),
+      ativa:true
+    }
+
+    //adicionar imagem ao array
+    this.images.push(objetoImagem)
+  }
+
+
+
+
 }
